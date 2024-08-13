010ea499af2e59c995d9d030ce11fa177f4321d7
Jihye Bae <jihye200311@gmail.com>
2024-08-13 13:22:57 +0100

    Organised songs that are in both streaming history and the playlists

--- streaming_history_data.py
+++ streaming_history_data.py
@@ -20,6 +20,7 @@ rename_podcast.loc[rename_podcast['trackName'] == "NewJeans (뉴진스) 'Bubble 
 
 # Concatenate 3 datarame
 all_history = pd.concat([df1, df2, rename_podcast])
+# print(all_history)
 
 # Drop column 'endTime'
 # drop_endTime = all_history.drop('endTime', axis=1)
@@ -27,6 +28,14 @@ all_history = pd.concat([df1, df2, rename_podcast])
 # Sum msPlayed if artistName and trackName are same
 sum_msPlayed = all_history.groupby(['trackName', 'artistName'], as_index=False)['msPlayed'].sum()
 # print(sum_msPlayed)
+
+# Get the latest endTime for trackName and artistName
+endTime = all_history.groupby(['trackName', 'artistName'], as_index=False)['endTime'].max()
+
+# Add 'endTime' column in 'sum_msPlayed'
+sum_msPlayed_with_endTime = pd.merge(sum_msPlayed, endTime, on=['trackName', 'artistName'], how='left')
+
+# print(sum_msPlayed_with_endTime)
 
 
 # Test with trackName 'Flower' if sum_msPlayed is working
@@ -39,12 +48,12 @@ sum_msPlayed = all_history.groupby(['trackName', 'artistName'], as_index=False)[
 
 
 # Sort sum_msPlayed by descending order
-sort_desc = sum_msPlayed.sort_values(by='msPlayed', ascending=False, ignore_index=True)
-print(sort_desc)
+sort_desc = sum_msPlayed_with_endTime.sort_values(by='msPlayed', ascending=False, ignore_index=True)
+# print(sort_desc)
 
 # Check if there is any empty values in 'trackName' and 'artistName'
 check_empty = sort_desc[['trackName', 'artistName']].isnull().sum()
-print(check_empty)
+# print(check_empty)
 
 # Because Json file is nested
 with open('Playlist1.json',encoding='UTF-8') as json_file:
@@ -58,5 +67,17 @@ filtered_playlists = df_flattened[df_flattened['name'].isin(["My playlist", "�
 # print(filtered_playlists)
 
 # Return only trackName and artistName
-item_names = filtered_playlists[['track.trackName', 'track.artistName']]
-print(item_names)
+item_names = filtered_playlists[['track.trackName', 'track.artistName', 'name']]
+item_names = item_names.sort_values(by='track.trackName', ignore_index=True)
+item_names = item_names.rename(columns={"track.trackName":"trackName", "track.artistName":"artistName", "name":"playlistName"})
+item_names = item_names.dropna(how='any', inplace=False)
+# print(item_names)
+
+# Get the songs that are in both streaming history and the playlists
+duplicated_playlist = pd.merge(sort_desc, item_names, how='right', on=['trackName', 'artistName'])
+duplicated_playlist = duplicated_playlist.sort_values(by='msPlayed', ascending=False, ignore_index=True)
+print(duplicated_playlist)
+
+# Change to excel file
+file_name = 'Playlist_organised.xlsx'
+duplicated_playlist.to_excel(file_name, index=False)
